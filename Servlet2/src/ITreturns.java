6eed47f6ceed6a7c160c19251b664e6bd377b6bd
ADITYA SINGH <95960794+adisingh25@users.noreply.github.com>
2023-04-13 12:12:36 +0530

    added file saving code

--- Servlet2/src/ITreturns.java
+++ Servlet2/src/ITreturns.java
@@ -1,5 +1,7 @@
 
 
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.PrintWriter;
 
@@ -33,11 +35,21 @@ public class ITreturns extends HttpServlet {
 		String gender = request.getParameter("gender");
 		String salary = request.getParameter("salary");
 		String tax = request.getParameter("tax");
-		int salaryAfterTax = Integer.parseInt(salary) - Integer.parseInt(tax);
+		int salaryAfterTax = Integer.parseInt(salary) - Integer.parseInt(salary)*Integer.parseInt(tax)/100;
 		PrintWriter out = response.getWriter();
 		out.print("<html><body><p>" + name + "</p><p>" + gender + "</p>");
 		out.print("<p>" + salary + "</p><p>" + tax + "</p><p>"+ salaryAfterTax  + "</body></html>");
 //		response.getWriter().append("Served at: ").append(request.getContextPath());
+		
+		
+		File file = new File("/home/6a/Desktop/output_tax/file.txt");
+		file.createNewFile();
+		FileOutputStream fs = new FileOutputStream(file);
+		System.out.println(fs.toString());
+		fs.write(("hello" + name + " " + gender + " your salary is " + salary + " and tax deduction % is "  
+		+ tax + " So, salary after deduction " + salaryAfterTax).getBytes());
+		fs.close();
+		
 	}
 
 
